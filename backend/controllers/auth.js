2721b9c6a3b2d7dbbc9427348552d6f6271ee749
Kristian Fulkerson <kristianf451@gmail.com>
2022-05-29 17:30:45 -0700

    feat: added User model, controllers/routes for auth with signin/signup, sign up working and tested in postman, returning a valid jwt token + user data

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -1,11 +1,80 @@
-const signup = (req, res) => {
-  res.json({ message: "Sign up" });
-};
-const signin = (req, res) => {
-  res.json({ message: "Sign in" });
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const asyncHandler = require("express-async-handler");
+
+const User = require("../models/user.js");
+
+const signin = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const existingUser = await User.findOne({ email });
+
+    if (!existingUser)
+      return res.status(404).json({ message: "User does not exist" });
+
+    const isPasswordCorrect = await bcrypt.compare(
+      password,
+      existingUser.password
+    );
+
+    if (!isPasswordCorrect)
+      return res.status(400).json({ message: "Invalid Credentials" });
+
+    const token = jwt.sign(
+      { email: existingUser.email, id: existingUser._id },
+      process.env.JWT_SECRET,
+      { expiresIn: "1h" }
+    );
+
+    res.status(200).json({ result: existingUser, token });
+  } catch (error) {
+    res.status(500).json({ message: "Something went wrong" });
+  }
 };
 
-module.exports = {
-  signin,
-  signup,
-};
+const signup = asyncHandler(async (req, res) => {
+  const { email, password, firstName, lastName, confirmPassword } = req.body;
+
+  if (!email || !password || !firstName || !lastName || !confirmPassword) {
+    res.status(400);
+    throw new Error("Please add all fields");
+  }
+
+  const existingUser = await User.findOne({ email });
+
+  if (existingUser) {
+    res.status(400);
+    throw new Error("User already exists");
+  }
+  if (password !== confirmPassword) {
+    res.status(400);
+    throw new Error("Passwords do not match");
+  }
+
+  const hashedPassword = await bcrypt.hash(password, 12);
+
+  const result = await User.create({
+    email,
+    password: hashedPassword,
+    name: `${firstName} ${lastName}`,
+  });
+
+  const token = jwt.sign({ email: result.email, id: result._id }, "secret", {
+    expiresIn: "1h",
+  });
+
+  if (result) {
+    res.status(201).json({
+      _id: result._id,
+      name: result.name,
+      email: result.email,
+      token: token,
+    });
+  } else {
+    res.status(400);
+    throw new Error("Invalid User data");
+  }
+});
+
+module.exports = { signin, signup };
