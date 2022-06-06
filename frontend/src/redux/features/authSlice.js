8445a0b5fbdd346df3f85ed7d00151986f876cda
Kristian Fulkerson <kristianf451@gmail.com>
2022-06-05 18:22:31 -0700

    feat: added api to setup all of our requests with axios. Added interceptors that will attach auth headers if a user if present in local storage

--- frontend/src/redux/features/authSlice.js
+++ frontend/src/redux/features/authSlice.js
@@ -1,7 +1,9 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+const user = JSON.parse(localStorage.getItem("user"));
+
 const initialState = {
-  user: null,
+  user: user ? user : null,
 };
 
 const authSlice = createSlice({
